a948b5e1bb315b2d8f88be5d883b7c7a29035791
mellic03 <mellic03@gmail.com>
2023-09-02 15:21:25 +1000

    Made wrong not wrong

--- attach.sh
+++ attach.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Docker attach doesn't work so use this instead.
+# ./attach <container name>
+
 docker exec -it $1 bash
--- frontend/repizza.sh
+++ frontend/repizza.sh
@@ -3,8 +3,8 @@
 # Stop and remove the pizzeria container and image, rebuild the image and start the container again.
 # Place and run from parent directory of pizzeria project
 
-docker stop frontend
-docker rm frontend
+docker stop node-frontend
+docker rm node-frontend
 docker rmi -f pizzera
 docker build -t pizzeria .
-docker run --network=chat-net --name=frontend -p 4200:4200 -d pizzeria
+docker run --network=chat-net --name=node-frontend -p 4200:4200 -d pizzeria
